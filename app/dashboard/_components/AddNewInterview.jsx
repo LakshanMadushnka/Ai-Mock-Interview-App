a53e3cc12933574bb48b95ebe97d75c0114b4941
LakshanMadushnka <lranasinghe286@gmail.com>
2025-01-02 18:51:43 +0530

    Generate Mock Interview Question with AI using Google Gemini AI

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -3,6 +3,13 @@ import React, {useState} from 'react'
 import {Button} from '@/components/ui/button';
 import {Input} from '@/components/ui/input';
 import {Textarea} from '@/components/ui/textarea';
+import {chatSession} from '@/utils/GeminAIModel';
+import {LoaderCircle} from 'lucide-react';
+import {db} from '@/utils/db';
+import {MockInverview} from  '@/utils/schema';
+import { v4 as uuidv4 } from 'uuid';
+import {useUser} from '@clerk/nextjs';
+import moment from 'moment';
 import {
     Dialog,
     DialogContent,
@@ -17,12 +24,48 @@ function AddNewInterview() {
     const [openDialog, setOpenDialog] = useState(false);
     const [jobPossition, setJobPossition] = useState();
     const [jobDescription, setJobDescription] = useState();
-    const [jobbExperience, setJobExperience] = useState();
-
-    const onSubmit = (e) => {
+    const [jobExperience, setJobExperience] = useState();
+    const [loading, setLoading] = useState(false);
+    const [jsonResponse, setJsonResponse] = useState([]);
+    const {user}=useUser();
+    const onSubmit = async(e) => {
+        setLoading(true)
         e.preventDefault()
-        console.log(jobPossition, jobDescription, jobbExperience);
+        console.log(jobPossition, jobDescription, jobExperience);
+        
+        const InputPrompt="Job Position: "+jobPossition+", Job Description: "+jobDescription+",  Years of Experience: "+jobExperience+", Depends on this Information please give us "+process.env.NEXT_PUBLIC_INTERVIEW_QUESTION_COUNT+" Interview question along with Answer in JSON Format, Give Question and Answered as field in JSON"
+        
+        const result = await chatSession.sendMessage(InputPrompt);
+        const MockJsonResp=(result.response.text()).replace('```json','').replace('```','');
+        console.log(JSON.parse(MockJsonResp));
+        setJsonResponse(MockJsonResp);
+
+        if(MockJsonResp){
+        const resp= await db.insert(MockInverview)
+        .values({
+            mockId: uuidv4(),
+            jsonMockResp:MockJsonResp,
+            jobPosition:jobPossition,
+            jobDesc: jobDescription,
+            jobExperience: jobExperience,
+            createdBy:user?.primaryEmailAddress?.emailAddress,
+            createdAt:moment().format('DD-MM-YYYY')
+        }).returning({mockId:MockInverview.mockId});
+
+        console.log("Insert ID:",resp);
+        
+        if(resp){
+            setOpenDialog(false);
+        }
+
+    else{
+        console.log("Error in AI Model");
     }
+
+        setLoading(false);
+    
+    }
+
 
   return (
     <div>
@@ -61,7 +104,12 @@ function AddNewInterview() {
 
                             <div className='flex gap-5 justify-end'>
                                 <Button type="button" variant="ghost" onClick={() => setOpenDialog(false)}>Cancel</Button>
-                                <Button type="submit">Start Interview</Button>
+                                <Button type="submit" disabled={loading}>
+                                    {loading ?
+                                    <>
+                                     <LoaderCircle className='animate-spin'/> 'Generating from AI'</>:'Start Interview'
+                                    }
+                                </Button>
                             </div>
                     </form>   
 
--- utils/schema.js
+++ utils/schema.js
@@ -3,9 +3,9 @@ import {pgTable, serial, text, varchar} from 'drizzle-orm/pg-core';
 export const MockInverview = pgTable('mockInterview', {
     id: serial('id').primaryKey(),
     jsonMockResp: text('jsonMockResp').notNull(),
-    jsonPosition: varchar('jsonPosition').notNull(),
-    jsonDesc: varchar('jsonDesc').notNull(),
-    jsonExperience: varchar('jsonExperience').notNull(),
+    jobPosition: varchar('jsonPosition').notNull(),
+    jobDesc: varchar('jsonDesc').notNull(),
+    jobExperience: varchar('jsonExperience').notNull(),
     createdBy: varchar('createdBy').notNull(),
     createdAt: varchar('createdAt'),
     mockId: varchar('mockId').notNull()
